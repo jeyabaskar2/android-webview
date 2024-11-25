d1f880295d18b569105f7db48665525e1d32fedf
Jeyabaskar <jeyabaskar005@gmail.com>
2024-11-25 20:21:58 +0530

    first commit

---
+++ app/src/main/java/com/eadsapps/pk/MainActivity.java
@@ -0,0 +1,134 @@
+package com.eadsapps.pk;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.ConnectivityManager.NetworkCallback;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.webkit.CookieManager;
+import android.webkit.URLUtil;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private WebView mWebView;
+    private NetworkCallback networkCallback;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        String base_url =this.getString(R.string.base_url);
+        mWebView.setWebViewClient(new HelloWebViewClient(this));
+
+        mWebView.setDownloadListener((url, userAgent, contentDisposition, mimetype, contentLength) -> {
+            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+            request.setMimeType(mimetype);
+            request.addRequestHeader("cookie", CookieManager.getInstance().getCookie(url));
+            request.addRequestHeader("User-Agent", userAgent);
+            request.setDescription("Downloading file...");
+            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype));
+            DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+            dm.enqueue(request);
+            Toast.makeText(getApplicationContext(), "Downloading File", Toast.LENGTH_LONG).show();
+        });
+
+        if (isNetworkAvailable()) {
+            mWebView.loadUrl(base_url);
+        } else {
+            mWebView.loadUrl("file:///android_asset/offline.html");
+        }
+
+        networkCallback = new NetworkCallback() {
+            @Override
+            public void onAvailable(Network network) {
+                runOnUiThread(() -> {
+                    if (!mWebView.getUrl().startsWith("file:///android_asset")) {
+                        mWebView.loadUrl(base_url);
+                    }
+                });
+            }
+            @Override
+            public void onLost(Network network) {
+                runOnUiThread(() -> {
+                    if (mWebView.getUrl() != null) {
+                        mWebView.loadUrl("file:///android_asset/offline.html");
+                    }
+                });
+            }
+        };
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        connectivityManager.registerDefaultNetworkCallback(networkCallback);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network nw = connectivityManager.getActiveNetwork();
+        if (nw == null) return false;
+        NetworkCapabilities actNw = connectivityManager.getNetworkCapabilities(nw);
+        return actNw != null && (actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) || actNw.hasTransport(NetworkCapabilities.TRANSPORT_VPN));
+    }
+
+    private static class HelloWebViewClient extends WebViewClient {
+        private final Context context;
+
+        // Constructor to pass context
+        public HelloWebViewClient(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            String url = request.getUrl().toString();
+            String baseUrl = context.getString(R.string.base_url);
+
+            // Check if the URL is outside the base domain
+            if (!url.startsWith(baseUrl)) {
+                // Open in the default browser
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                context.startActivity(intent);
+                return true; // Indicate that the URL is handled externally
+            } else {
+                view.loadUrl(url); // Load within the WebView for matching URLs
+                return false;
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (networkCallback != null) {
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            connectivityManager.unregisterNetworkCallback(networkCallback);
+        }
+    }
+}
